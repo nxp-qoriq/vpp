04f5f43a89fb8a05576d3d3785e2747ccd0d3424
Apeksha Gupta <apeksha.gupta@nxp.com>
2022-05-20 16:11:59 +0530

    dpdk: disable cryptodev
    Signed-off-by: Apeksha Gupta <apeksha.gupta@nxp.com>
    DPDK-3417

--- src/plugins/dpdk/device/init.c
+++ src/plugins/dpdk/device/init.c
@@ -27,7 +27,7 @@
 #include <vnet/interface/tx_queue_funcs.h>
 #include <dpdk/buffer.h>
 #include <dpdk/device/dpdk.h>
-#include <dpdk/cryptodev/cryptodev.h>
+//#include <dpdk/cryptodev/cryptodev.h>
 #include <vlib/pci/pci.h>
 #include <vlib/vmbus/vmbus.h>
 
@@ -1449,7 +1449,7 @@ dpdk_process (vlib_main_t * vm, vlib_node_runtime_t * rt, vlib_frame_t * f)
 
   if (error)
     clib_error_report (error);
-
+#if 0
   if (dpdk_cryptodev_init)
     {
       error = dpdk_cryptodev_init (vm);
@@ -1460,7 +1460,7 @@ dpdk_process (vlib_main_t * vm, vlib_node_runtime_t * rt, vlib_frame_t * f)
 	  clib_error_free (error);
 	}
     }
-
+#endif
   tm->worker_thread_release = 1;
 
   f64 now = vlib_time_now (vm);
